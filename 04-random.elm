085223e439c9cf4482928b9961614bcf36df6952
Dominique VASSARD <dominique.vassard@gmail.com>
2017-02-19 17:47:43 +0100

    04-random with svg dice

--- 04-random.elm
+++ 04-random.elm
@@ -3,11 +3,18 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (src, width, height)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 import Random
 
 
 main =
-    Html.program { init = init, update = update, subscriptions = subscriptions, view = view }
+    Html.program
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
 
 
@@ -32,7 +39,10 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Roll ->
-            ( model, Random.generate NewFace (Random.list (List.length model.dieFaces) (Random.int 1 6)) )
+            ( model
+            , Random.list (List.length model.dieFaces) (Random.int 1 6)
+                |> Random.generate NewFace
+            )
 
         NewFace newFace ->
             ( Model newFace, Cmd.none )
@@ -50,18 +60,184 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text "Roll the dice" ]
+        [ h1 [] [ Html.text "Roll the dice" ]
         , br []
             []
-        , div [] (List.map viewDice model.dieFaces)
+        , div [] (List.map viewDiceSvg model.dieFaces)
         , br [] []
-        , button [ onClick Roll ] [ text "Roll" ]
+        , button [ onClick Roll ] [ Html.text "Roll" ]
         ]
 
 
-viewDice : Int -> Html Msg
-viewDice dieface =
-    img [ src ("./images/Dice-" ++ (toString dieface) ++ ".png"), width 250, height 250 ] []
+viewDiceSvg : Int -> Html Msg
+viewDiceSvg dieface =
+    let
+        size =
+            200
+
+        margin =
+            10
+
+        fill_color =
+            "orange"
+
+        stroke_color =
+            "black"
+
+        stroke_width =
+            5
+    in
+        svg
+            [ viewBox ("0 0 " ++ toString size ++ " " ++ toString size)
+            , Svg.Attributes.width (toString size)
+            , Svg.Attributes.height (toString size)
+            ]
+            [ rect
+                [ x (toString margin)
+                , y (toString margin)
+                , Svg.Attributes.width (toString (size - 2 * margin))
+                , Svg.Attributes.height (toString (size - 2 * margin))
+                , rx (toString margin)
+                , ry (toString margin)
+                , stroke stroke_color
+                , strokeWidth (toString stroke_width)
+                , fill fill_color
+                ]
+                []
+            , viewPip dieface size margin
+            ]
+
+
+
+-- Draw pipe for the given number
+
+
+viewPip : Int -> Int -> Int -> Html Msg
+viewPip dieface size margin =
+    let
+        pip_size =
+            size * 8 // 100
+
+        fill_color =
+            "green"
+
+        -- Dice is a square then each measurement fits vertical and horizontal
+        step =
+            (size - 2 * margin) // 4
+
+        first =
+            margin // 2 + step
+
+        middle =
+            first + step
+
+        last =
+            middle + step
+
+        pip_top_left =
+            Svg.circle
+                [ cx (toString first)
+                , cy (toString first)
+                , r (toString pip_size)
+                ]
+                []
+
+        pip_bottom_left =
+            Svg.circle
+                [ cx (toString first)
+                , cy (toString last)
+                , r (toString pip_size)
+                ]
+                []
+
+        pip_top_right =
+            Svg.circle
+                [ cx (toString last)
+                , cy (toString first)
+                , r (toString pip_size)
+                ]
+                []
+
+        pip_bottom_right =
+            Svg.circle
+                [ cx (toString last)
+                , cy (toString last)
+                , r (toString pip_size)
+                ]
+                []
+
+        pip_center =
+            Svg.circle
+                [ cx (toString middle)
+                , cy (toString middle)
+                , r (toString pip_size)
+                ]
+                []
+
+        pip_center_left =
+            Svg.circle
+                [ cx (toString first)
+                , cy (toString middle)
+                , r (toString pip_size)
+                ]
+                []
+
+        pip_center_right =
+            Svg.circle
+                [ cx (toString last)
+                , cy (toString middle)
+                , r (toString pip_size)
+                ]
+                []
+    in
+        case (clamp 1 6 dieface) of
+            1 ->
+                Svg.g [ fill fill_color ]
+                    [ pip_center
+                    ]
+
+            2 ->
+                Svg.g [ fill fill_color ]
+                    [ pip_top_left
+                    , pip_bottom_right
+                    ]
+
+            3 ->
+                Svg.g [ fill fill_color ]
+                    [ pip_top_left
+                    , pip_center
+                    , pip_bottom_right
+                    ]
+
+            4 ->
+                Svg.g [ fill fill_color ]
+                    [ pip_top_left
+                    , pip_bottom_left
+                    , pip_top_right
+                    , pip_bottom_right
+                    ]
+
+            5 ->
+                Svg.g [ fill fill_color ]
+                    [ pip_top_left
+                    , pip_bottom_left
+                    , pip_center
+                    , pip_top_right
+                    , pip_bottom_right
+                    ]
+
+            6 ->
+                Svg.g [ fill fill_color ]
+                    [ pip_top_left
+                    , pip_center_left
+                    , pip_bottom_left
+                    , pip_top_right
+                    , pip_center_right
+                    , pip_bottom_right
+                    ]
+
+            _ ->
+                Debug.crash "Impossible"
 
 
 
@@ -70,4 +246,4 @@ viewDice dieface =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model (List.repeat 2 1), Cmd.none )
+    ( Model (List.repeat 5 1), Cmd.none )
