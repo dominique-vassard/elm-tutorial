59810b15d2bcc9c063521c4bdeb865b6ccdd5322
Dominique VASSARD <dominique.vassard@gmail.com>
2017-02-23 00:44:03 +0100

    06-time: Add hours and minutes

--- 06-time.elm
+++ 06-time.elm
@@ -1,9 +1,10 @@
 module Main exposing (..)
 
-import Html exposing (Html)
+import Html exposing (Html, div, text)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
+import Date
 
 
 main : Program Never Model Msg
@@ -61,15 +62,45 @@ view : Model -> Html Msg
 view model =
     let
         angle =
-            turns (Time.inMinutes model)
+            --turns (Time.inMinutes model)
+            Basics.degrees (Basics.toFloat ((Date.second <| Date.fromTime model) * 6) - 90.0)
 
         handX =
-            toString (50 + 40 * cos angle)
+            toString (50 + 35 * cos angle)
 
         handY =
-            toString (50 + 40 * sin angle)
+            toString (50 + 35 * sin angle)
+
+        angleH =
+            Basics.degrees (Basics.toFloat ((Date.hour <| Date.fromTime model) * 30) - 90.0)
+
+        handHX =
+            toString (50 + 20 * cos angleH)
+
+        handHY =
+            toString (50 + 20 * sin angleH)
+
+        angleM =
+            Basics.degrees (Basics.toFloat ((Date.minute <| Date.fromTime model) * 6) - 90.0)
+
+        handMX =
+            toString (50 + 40 * cos angleM)
+
+        handMY =
+            toString (50 + 40 * sin angleM)
+
+        minutes =
+            Time.inMinutes model
+
+        seconds =
+            "///" ++ toString (Date.second <| Date.fromTime model)
     in
-        svg [ viewBox "0 0 100 100", width "300px" ]
-            [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-            , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+        div []
+            [ svg
+                [ viewBox "0 0 100 100", width "300px" ]
+                [ circle [ cx "50", cy "50", r "45", fill "#6B7912" ] []
+                , line [ x1 "50", y1 "50", x2 handMX, y2 handMY, stroke "black" ] []
+                , line [ x1 "50", y1 "50", x2 handHX, y2 handHY, stroke "black" ] []
+                , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "red" ] []
+                ]
             ]
